188da8e6458c254ddc62f9ee781fef2c15f8f94f
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2023-03-26 17:30:44 +0200

    Refactored repository to follow the Julia module creation guide.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ELM
 
 [![Build Status](https://github.com/sergiovaneg/ELM.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/sergiovaneg/ELM.jl/actions/workflows/CI.yml?query=branch%3Amain)
+
+Extreme Learning Machine implementation in Julia
---
+++ src/neurons.jl
@@ -0,0 +1,23 @@
+function sigmoid(x::Array{Float64,1})
+    return 1.0 ./ (1.0 .+ exp.(-x));
+end
+
+function gaussian(x::Array{Float64,1})
+    return exp.(-(x .^ 2));
+end
+
+struct AbstractNeuron
+    g::Function
+end
+
+struct RBF_neuron{T <: Real} <: AbstractNeuron
+    a::Vector{T}
+    b::T
+    act_fcn::Function
+    g::Function
+
+    function RBF_neuron{T}(a::Vector{T}, b::T, act_fcn::Function) where T<:Real
+        g(x::Matrix{T}) = act_fcn.(b.*LinearAlgebra.norm.(eachrow(x.-a')));
+        new(a, b, act_fcn, g);
+    end 
+end
