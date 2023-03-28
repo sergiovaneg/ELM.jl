fe5514ef97a9703079e9ac1a1ca8a40aa706c434
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2023-03-28 10:34:59 +0200

    Refactored operator responsability and fixed style.

--- src/neurons.jl
+++ src/neurons.jl
@@ -1,23 +1,35 @@
 import LinearAlgebra
 
-function sigmoid(x::Array{Float64,1})
-    return 1.0 ./ (1.0 .+ exp.(-x))
+function sigmoid(x::Real)
+    return 1.0 / (1.0 + exp(-x))
 end
 
-function gaussian(x::Array{Float64,1})
-    return exp.(-(x .^ 2))
+function gaussian(x::Real)
+    return exp(-(x ^ 2))
 end
 
-abstract type AbstractNeuron{T <: Real} end
+abstract type AbstractNeuron{T<:Real} end
 
-struct RBF_neuron{T <: Real} <: AbstractNeuron{T}
+struct RBF_neuron{T<:Real} <: AbstractNeuron{T}
     a::Vector{T}
     b::T
     act_fcn::Function
     g::Function
 
     function RBF_neuron{T}(a::Vector{T}, b::T, act_fcn::Function) where T<:Real
-        g(x::Matrix{T}) = act_fcn.(b.*LinearAlgebra.norm.(eachrow(x.-a')));
+        g(x::Vector{T}) = act_fcn(b * LinearAlgebra.norm(x - a));
         new(a, b, act_fcn, g);
     end 
+end
+
+struct ADD_neuron{T<:Real} <: AbstractNeuron{T}
+    a::Vector{T}
+    b::T
+    act_fcn::Function
+    g::Function
+
+    function ADD_neuron{T}(a::Vector{T}, b::T, act_fcn::Function) where T<:Real
+        g(x::Vector{T}) = act_fcn(LinearAlgebra.dot(x, a) + b);
+        new(a, b, act_fcn, g);
+    end
 end
